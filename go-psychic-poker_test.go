31025b9a9c854402f05c9bae4c58fff73e38d174
Wouter Schut <wouter.schut@philips.com>
2018-02-06 20:13:06 +0100

    Add TestProcessString

--- go-psychic-poker_test.go
+++ go-psychic-poker_test.go
@@ -2,20 +2,17 @@ package main
 
 import (
 	"testing"
-
-	"github.com/seweso/stringutil"
 )
 
-func TestReverse(t *testing.T) {
+func TestProcessString(t *testing.T) {
 	cases := []struct {
 		in, want string
 	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{"Hello, 世界", "界世 ,olleH"},
-		{"", ""},
+		{"TH JH QC QD QS QH KH AH 2S 6S", "Hand: TH JH QC QD QS Deck: QH KH AH 2S 6S Best hand: straight-flush"},
+		{"2H 2S 3H 3S 3C 2D 3D 6C 9C TH", "Hand: 2H 2S 3H 3S 3C Deck: 2D 3D 6C 9C TH Best hand: four-of-a-kind"},
 	}
 	for _, c := range cases {
-		got := stringutil.Reverse(c.in)
+		got := ProcessString(c.in)
 		if got != c.want {
 			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
 		}
