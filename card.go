0ed4bb987ff665405c54c341b543a6b345d68b1b
Wouter Schut <wouter.schut@philips.com>
2018-02-06 22:22:21 +0100

    Implement card to string

--- card.go
+++ card.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 type card struct {
 	f face
 	s suit
+}
+
+func (c card) String() string {
+	return fmt.Sprintf("%v of %v", c.f, c.s)
+}
+
+func (c card) ShortString() string {
+	return fmt.Sprintf("%v%v", string(c.f.String()[0]), string(c.s.String()[0]))
 }
--- card_test.go
+++ card_test.go
@@ -19,6 +19,40 @@ func TestFaceString(t *testing.T) {
 	}
 }
 
+func TestCardString(t *testing.T) {
+	cases := []struct {
+		in   card
+		want string
+	}{
+		{card{faceAce, diamonds}, "Ace of Diamonds"},
+		{card{faceQueen, spades}, "Queen of Spades"},
+	}
+
+	for _, c := range cases {
+		got := c.in.String()
+		if got != c.want {
+			t.Errorf("card.String(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func TestCardStringShort(t *testing.T) {
+	cases := []struct {
+		in   card
+		want string
+	}{
+		{card{faceAce, diamonds}, "AD"},
+		{card{faceQueen, spades}, "QS"},
+	}
+
+	for _, c := range cases {
+		got := c.in.ShortString()
+		if got != c.want {
+			t.Errorf("card.String(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
 func TestSuitConvert(t *testing.T) {
 	cases := []struct {
 		in   byte
