ed9e5bcdb8a317ee8d834f6efe682e5c8ee6b15b
Wouter Schut <wouter.schut@philips.com>
2018-02-06 19:10:19 +0100

    Add example test case (external string util)

---
+++ go-psychic-poker_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/seweso/stringutil"
+)
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := stringutil.Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
